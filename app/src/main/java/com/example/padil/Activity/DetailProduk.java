3144ebd3badff75dd6738035212a4861f13329ac
Danil Gans <lastdanil@gmail.com>
2023-02-13 03:11:09 +0700

    Update

--- app/src/main/java/com/example/padil/Activity/DetailProduk.java
+++ app/src/main/java/com/example/padil/Activity/DetailProduk.java
@@ -1,23 +1,36 @@
 package com.example.padil.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.padil.Model.ProdukPopulerModel;
 import com.example.padil.Model.SemuaProdukModel;
 import com.example.padil.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class DetailProduk extends AppCompatActivity {
 
     ImageView imgProduk;
-    TextView namaProduk, hargaProduk, deskripsiProduk;
+    TextView namaProduk, hargaProduk, deskripsiProduk, Kuantiti;
     ImageView addItem, removeItem, addtoCart, buyNow, backBtnDP;
+
+    int totalKuantiti = 1;
+    int totalHarga = 0;
 
     //Produk Populer
     ProdukPopulerModel produkPopulerModel = null;
@@ -25,6 +38,7 @@ public class DetailProduk extends AppCompatActivity {
     //Semua Produk
     SemuaProdukModel semuaProdukModel = null;
 
+    FirebaseAuth auth;
     private FirebaseFirestore firestore;
 
     @Override
@@ -33,6 +47,7 @@ public class DetailProduk extends AppCompatActivity {
         setContentView(R.layout.activity_detail_produk);
 
         firestore = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
 
         final Object obj = getIntent().getSerializableExtra("detailproduk");
 
@@ -46,6 +61,7 @@ public class DetailProduk extends AppCompatActivity {
         namaProduk = findViewById(R.id.nama_produk);
         hargaProduk = findViewById(R.id.harga_produk);
         deskripsiProduk = findViewById(R.id.isi_desk);
+        Kuantiti = findViewById(R.id.kuantiti);
 
         addItem = findViewById(R.id.additem);
         removeItem = findViewById(R.id.removeitem);
@@ -67,16 +83,85 @@ public class DetailProduk extends AppCompatActivity {
             namaProduk.setText(produkPopulerModel.getNama());
             hargaProduk.setText(String.valueOf(produkPopulerModel.getHarga()));
             deskripsiProduk.setText(produkPopulerModel.getDeskripsi());
+
+            totalHarga = produkPopulerModel.getHarga() * totalKuantiti;
         }
 
-        //Semua Prduk
+        //Semua Produk
 
         if (semuaProdukModel != null){
             Glide.with(getApplicationContext()).load(semuaProdukModel.getImg_url()).into(imgProduk);
             namaProduk.setText(semuaProdukModel.getNama());
             hargaProduk.setText(String.valueOf(semuaProdukModel.getHarga()));
             deskripsiProduk.setText(semuaProdukModel.getDeskripsi());
+
+            totalHarga = semuaProdukModel.getHarga() * totalKuantiti;
         }
+
+        //Tambah ke keranjang
+        addtoCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String saveCurrentTime, saveCurrentDate;
+
+                Calendar callForDate = Calendar.getInstance();
+
+                SimpleDateFormat currentDate = new SimpleDateFormat("MM dd, yyyy");
+                saveCurrentDate = currentDate.format(callForDate.getTime());
+
+                SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
+                saveCurrentTime = currentTime.format(callForDate.getTime());
+
+                final HashMap<String, Object> cartMap = new HashMap<>();
+
+                cartMap.put("namaProduk", namaProduk.getText().toString());
+                cartMap.put("hargaProduk", hargaProduk.getText().toString());
+                cartMap.put("currentDate", saveCurrentDate);
+                cartMap.put("currentTime", saveCurrentTime);
+                cartMap.put("totalKuantiti", Kuantiti.getText().toString());
+                cartMap.put("totalHarga", totalHarga);
+
+                firestore.collection("Keranjang").document
+                        (auth.getCurrentUser().getUid())
+                        .collection("User").add(cartMap).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                            @Override
+                            public void onComplete(@NonNull Task<DocumentReference> task) {
+                                Toast.makeText(DetailProduk.this, "Berhasil memasukan ke keranjang", Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+                        });
+            }
+        });
+
+        //Tambah Kuantiti barang
+        addItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (totalKuantiti < 10){
+                    totalKuantiti++;
+                    Kuantiti.setText(String.valueOf(totalKuantiti));
+
+                    if (produkPopulerModel != null){
+                        totalHarga = produkPopulerModel.getHarga() * totalKuantiti;
+                    }
+                    if (semuaProdukModel != null){
+                        totalHarga = semuaProdukModel.getHarga() * totalKuantiti;
+                    }
+                }
+            }
+        });
+
+        //Mengurangi Kuantiti barang
+        removeItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (totalKuantiti > 1){
+                    totalKuantiti--;
+                    Kuantiti.setText(String.valueOf(totalKuantiti));
+                }
+            }
+        });
 
     }
 }
